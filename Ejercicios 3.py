fda3b5bdab866b03b48fed2a0fae96017c66f5a7
Erick-Mercedes <68484028+Erick-Mercedes@users.noreply.github.com>
2020-10-31 16:35:16 -0400

    Delete Ejercicios 3.py

--- Ejercicios 3.py
+++
@@ -1,8 +0,0 @@
-# Pedir peso y altura para calcular la masa corporal: mc = peso / altura^2.
-
-P = float(input("Digita su peso actual: "))
-A = float(input("Digita su peso actual: "))
-
-mc = (P / A)**2
-
-print (mc)
